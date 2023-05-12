6660734b14ab5fc8d91bb1051e0b489fe12346e5
Fatima Garcia <fagarmartin@gmail.com>
2023-05-12 18:16:40 +0200

    Terminar editar libro, borrar libro y despelleje

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,60 +1,97 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/Book.model.js");
-const uploader = require("../middlewares/cloudinary.middleware.js")
-const {isAdmin}= require("../middlewares/auth.middlewares.js")
-
+const uploader = require("../middlewares/cloudinary.middleware.js");
+const { isAdmin } = require("../middlewares/auth.middlewares.js");
 
 //GET "/admin/index-admin" => renderiza la vista de la pagina de administrador
-router.get("/index-admin",isAdmin, async (req, res, next) => {
-  try{
-   
-    const allBooks =  await Book.find()
-    res.render("admin/index-admin",{
-      allBooks
+router.get("/index-admin", isAdmin, async (req, res, next) => {
+  try {
+    const allBooks = await Book.find();
+    res.render("admin/index-admin", {
+      allBooks,
     });
-  }catch(error){
-    next(error)
+  } catch (error) {
+    next(error);
   }
-
 });
 
 //GET "/admin/create" => renderiza la vista del formulario de crear libros
-router.get("/create",isAdmin, (req, res, next) => {
+router.get("/create", isAdmin, (req, res, next) => {
   res.render("admin/create");
 });
 
 //POST "/admin/create" => recoge los datos del formulario de creacion de libros
 router.post("/create", uploader.single("image"), async (req, res, next) => {
-if(req.file === undefined){
-  next("no hay imagen")
-  return
-}
+  if (req.file === undefined) {
+    next("no hay imagen");
+    return;
+  }
 
- try {
-    const {title,synopsis, numPag,author,genre} = req.body
+  try {
+    const { title, synopsis, numPag, author, genre } = req.body;
     Book.create({
       title,
       author,
       synopsis,
       numPag,
       genre,
-      image: req.file.path
-    })
-    console.log("libro añadido")
-    res.redirect("/admin/create") // cambiar ruta de redireccion despues de crear BD
-   } catch (error) {
-     next(error);
-   }
+      image: req.file.path,
+    });
+    console.log("libro añadido");
+    res.redirect("/admin/create"); // cambiar ruta de redireccion despues de crear BD
+  } catch (error) {
+    next(error);
+  }
 });
 
+//todo en la vista seleccionar el genero que esté en la base de datos
+//GET "/admin/:id/edit" => renderiza la vista del formulario
+router.get("/:id/edit", isAdmin, async (req, res, next) => {
+  try {
+    const bookDetails = await Book.findById(req.params.id);
+    res.render("admin/edit", { bookDetails });
+  } catch (error) {
+    next(error);
+  }
+});
 
+//POST "/admin/:id/edit" => recibe la informacion del formulario de editar
+//  uploader.single("image"),
+router.post(  "/:id/edit",  isAdmin,async (req, res, next) => {
+    const { title, synopsis, numPag, author, genre } = req.body;
+    try {
+      const editBooks = await Book.findByIdAndUpdate(
+        req.params.id,
+        {
+          title,
+          synopsis,
+          numPag,
+          author,
+          genre,
+         // image: req.file.path,
+        },
+        { new: true }
+      );
+      res.redirect("/admin/index-admin");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
+//POST "/admin/:id/delete" => borrar libro por su id
+router.post("/:id/delete",isAdmin,async(req,res,next)=>{
+try{
 
+  const bookDeleted=await Book.findByIdAndDelete(req.params.id)
+  res.redirect("/admin/index-admin")
+}
+catch(error)
+{
+  next(error)
+}
 
-//GET "/admin/editDelete" => renderiza la vista del formulario
-router.get("/editDelete", isAdmin, (req, res, next)=>{
-  res.render("admin/edit-delete")
 })
 
 module.exports = router;
