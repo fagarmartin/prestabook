412a4cae558097adffe379be17230eed48b5296e
Fatima Garcia <fagarmartin@gmail.com>
2023-05-15 12:57:03 +0200

    Terminar busqueda y vista de busqueda

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,37 +3,43 @@ const router = express.Router();
 
 const Book = require("../models/Book.model.js");
 
-// GET "/book/:id"=> renderiza los detalles del libro
-
-router.get("/:id", async(req, res, next)=>{
-    try{
-         const bookDetails = await Book.findById(req.params.id)
-        res.render("book/index", {
-            bookDetails
-        })
-    }
-catch(error){
-    next(error)
-}
-   
-})
-
 // GET "/book/search-list" => renderiza la vista de buscar libro
 
-router.get("/search-list", async(req, res, next)=>{
-    try{
-        const searchBook = await Book.findOne({title: req.query.title, author: req.query.author})
-        console.log(searchBook)
-        res.render("book/search-list",{
-            searchBook
-            
-        })
+// GET "/book/:id"=> renderiza los detalles del libro
 
-    }
-    catch(error){
-        next(error)
-    }
-})
+router.get("/:id/details", async (req, res, next) => {
+  console.log("ENTRA EN :id");
+  try {
+    const bookDetails = await Book.findById(req.params.id);
+    res.render("book/index", {
+      bookDetails,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
+//! lo metemos dentro de /search porque si no coge la primera ruta que haya dentro de /book
+router.get("/search/search-list", async (req, res, next) => {
+  try {
+    //  const searchBook = await Book.findOne({title: req.query.title})
+    const searchBook = await Book.find(
+     /* title: { $regex: req.query.datosBusqueda.toLowerCase()},
+      author: { $regex: req.query.datosBusqueda.toLowerCase() } ,
+      genre: { $regex: req.query.datosBusqueda.toLowerCase()  }, */
+
+      { $or: [ {title: { $regex: req.query.datosBusqueda.toLowerCase()}}, {author: { $regex: req.query.datosBusqueda.toLowerCase()}},{genre: { $regex: req.query.datosBusqueda.toLowerCase()  }} ] }
+    );
+
+    console.log(searchBook);
+    res.render("book/search-list", {
+      searchBook,
+    });
+
+   
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
